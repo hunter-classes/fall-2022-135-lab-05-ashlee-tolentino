c7944cf5c6e61bdef8964437c47db34bd6af986c
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-10-05 19:26:14 -0400

    task a done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab5
+
+This program 
+*/
+
 #include <iostream>
-
-
 #include "funcs.h"
 
 int main()
 {
+  bool result1 = isDivisibleBy(100, 25);
+  std::cout << result1 << std::endl;
+
+  
+  
   return 0;
 }
