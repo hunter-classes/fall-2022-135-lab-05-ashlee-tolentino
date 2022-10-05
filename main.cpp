c29b819951422452859dc6b2aa93f0abfd3f65c1
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-10-05 19:51:05 -0400

    task b done

--- main.cpp
+++ main.cpp
@@ -12,10 +12,12 @@ This program
 
 int main()
 {
+  std::cout << "In main\n";
   bool result1 = isDivisibleBy(100, 25);
   std::cout << result1 << std::endl;
 
-  
+  bool result2 = isPrime(47);
+  std::cout << result2 << std::endl;
   
   return 0;
 }
