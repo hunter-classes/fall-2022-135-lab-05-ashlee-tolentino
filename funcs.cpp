d81177d2dd522adeb83a88e06b40055a5b6a6962
Ashlee Tolentino <ashleetolentino@Ashlees-Air.lan>
2022-10-05 19:26:52 -0400

    task a done

--- funcs.cpp
+++ funcs.cpp
@@ -1,5 +1,25 @@
+/*
+Author: Ashlee Tolentino
+Course: CSCI-135
+Instructor: Michael Zamansky
+Assignment: Lab5
+
+This program
+*/
+
 #include <iostream>
 #include "funcs.h"
 
-// add functions here
+//This function returns true if n is divisible by d, and it returns false otherwise.
+bool isDivisibleBy(int n, int d)
+{
+  if(n % d == 0)
+  {
+    return true;
+  }
+  else
+  {
+    return false;
+  }
+}
 
